b156c686c8bfe965aea19e34f0282fc48b4fe0c7
LucaVigato <luca.vigato72@gmail.com>
2021-07-01 12:10:56 +0200

    Initial Commit

---
+++ Battery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Battery
+//
+//  Created by Luca Vigato on 01/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
